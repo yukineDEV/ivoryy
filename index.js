eb5dc6d1554a79878f10afafd189e7ee07881441
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2021-03-01 09:04:06 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["(your glitch link)"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
