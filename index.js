b3e6066c4d07b8b87222c60ec85446d0e6d2305c
yukineDEV <89830176+yukineDEV@users.noreply.github.com>
2021-09-06 08:11:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["(your glitch link)"]
+const urls = ["https://project-2-yukine.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
